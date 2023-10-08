6bfa831931d85533669f6ee18b0cfa2229dd1fb9
ganddi <kooganddi@gmail.com>
2023-10-08 12:33:22 +0800

    first commit

---
+++ Day1_welcome_to_java.java
@@ -0,0 +1,9 @@
+package day1_welcome_to_java;
+public class Day1_welcome_to_java {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+    
+    
+}

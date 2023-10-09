bd3d2ca0109c216c4843cc0312dd6e42ddc392ac
ganddi <kooganddi@gmail.com>
2023-10-09 14:30:43 +0800

    day1

--- Day1_welcome_to_java.java
+++
@@ -1,9 +0,0 @@
-package day1_welcome_to_java;
-public class Day1_welcome_to_java {
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-    
-    
-}

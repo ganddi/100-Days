7d97d1bb188737955139f9ecdb1bf7c0b7d8c88d
ganddi <kooganddi@gmail.com>
2023-10-15 17:11:11 +0800

    variable

---
+++ day 7.java
@@ -0,0 +1,43 @@
+package day.pkg7;
+public class Day7 {
+    public static void main(String[] args) {
+    //Variable Deklarasi Tanpa inisialisasi
+    String nama;
+    int angka;
+    boolean fakta;
+    
+    //variable Deklarasi dengan inisialisasi
+    String kampus = "unsulbar";
+    int umur = 18;
+    boolean angkata23 = true;
+    
+    //variable inisialisasi di blockcode
+    int angkatan;
+    angkatan = 2023;
+    double ukt;
+    ukt = 2500000;
+        System.out.println("angkatan: " + angkatan);
+        System.out.println("ukt: " + ukt);
+    
+    //variable deklarasi bersamaan
+    byte a, b, c;
+    a = 2;
+    b = 4;
+    c = 6;
+    
+    double x, y, z;
+    x = 0.5;
+    y = 10.5;
+    z = 7.5;
+    
+    //variable update
+    int nilai = 50;
+    nilai += 25;
+        System.out.println("nilai: "+nilai);
+        
+    //variable final atau konstan
+    final int sks = 20;
+        System.out.println("SKS: " + sks);
+    }
+   
+}

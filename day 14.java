1070172c0cbf7c5186c4a973b7a7ef36f76f21b9
ganddi <kooganddi@gmail.com>
2023-10-22 22:04:09 +0800

    Perulangan For

---
+++ day 14.java
@@ -0,0 +1,12 @@
+package day.pkg14;
+
+import java.util.Scanner;
+
+public class Day14 {
+    public static void main(String[] args) {
+      for (int i = 1; i <= 5; i++) {
+    System.out.println(i);
+}
+    }
+    
+}

c78d02e6c7ce57887b260f400992ee3e28b84714
ganddi <kooganddi@gmail.com>
2023-10-23 14:28:55 +0800

    for down to

---
+++ day 15.java
@@ -0,0 +1,9 @@
+package day.pkg15;
+public class Day15 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    
+}

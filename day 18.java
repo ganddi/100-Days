abee3cf662800eb1dbd63a9b131d1a9e0c4a12f5
ganddi <kooganddi@gmail.com>
2023-10-26 21:50:23 +0800

    for decrement

---
+++ day 18.java
@@ -0,0 +1,9 @@
+package day.pkg18;
+public class Day18 {
+    public static void main(String[] args) {
+        for (int i = 60; i >= 3; i-=3) {
+            System.out.println(i);
+        }
+    }
+    
+}

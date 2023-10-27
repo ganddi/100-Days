ee857f8f10859f2bd2c27fbca3b4e054cd08f506
ganddi <kooganddi@gmail.com>
2023-10-27 08:43:29 +0800

    while

---
+++ day 19.java
@@ -0,0 +1,11 @@
+package day.pkg19;
+public class Day19 {
+    public static void main(String[] args) {
+        int angkaGenap = 2;
+        while (angkaGenap <=  17){
+            System.out.println(angkaGenap);
+            angkaGenap+=2;
+        }
+    }
+    
+}

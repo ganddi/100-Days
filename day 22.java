17580696208f98542709aea3528250fdc1afe176
ganddi <kooganddi@gmail.com>
2023-10-30 14:11:16 +0800

    Do while

---
+++ day 22.java
@@ -0,0 +1,11 @@
+package day.pkg22;
+public class Day22 {
+    public static void main(String[] args) {
+     
+        int ganjil = 1;
+        
+        do{System.out.println("Angka" + ganjil);
+           ganjil+=2;
+     }while( ganjil <= 20);   
+    }
+}

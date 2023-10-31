3da3ce1899b6af7faf3dbf8d21528bf935d38910
ganddi <kooganddi@gmail.com>
2023-10-31 08:47:35 +0800

    infinite loop

---
+++ Day23.java
@@ -0,0 +1,11 @@
+package day.pkg23;
+public class Day23 {
+    public static void main(String[] args) {
+       for (;;) {
+            System.out.println("HAHA");     
+} 
+        while (true) {            
+            System.out.println("WKWK");
+        }
+    } 
+}

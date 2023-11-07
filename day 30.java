3a574bad7804395b84dfb217ec25065725cd5346
ganddi <kooganddi@gmail.com>
2023-11-07 20:02:35 +0800

    Perulangan, Mencari Hasil Dari 3^4

---
+++ day 30.java
@@ -0,0 +1,13 @@
+public class day30 {
+    public static void main(String[] args) {      
+   /*Tulislah program dengan bahasa java untuk menghitung hasil dari  3^4 !
+        *Gunakan konsep perulangan untuk menyelesaikannya
+    */    
+    
+        int angka = 3;
+        for (int i = 1; i <= 3; i++) {
+            angka *= 3;
+        }
+         System.out.println(angka);
+        }   
+    }

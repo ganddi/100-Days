c47ee3fa85a09b3ab95c7319dc6c6b8907150a4a
ganddi <kooganddi@gmail.com>
2023-11-09 22:37:30 +0800

    Deklarasi Array

---
+++ day 32.java
@@ -0,0 +1,13 @@
+public class day32 {
+    public static void main(String[] args) {
+      int[] angka = new int[4]; 
+        
+        angka[0] = 10;
+        angka[1] = 20;
+        angka[2] = 30;
+        angka[3] = 40;
+            
+      System.out.println(angka[2]);
+    }
+    
+ }

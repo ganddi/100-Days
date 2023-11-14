77d884dce18d8054dd8b96957c1f73e1b7823c73
ganddi <kooganddi@gmail.com>
2023-11-14 22:46:27 +0800

    pola segiempat dengan perulangan for

---
+++ day 37.java
@@ -0,0 +1,10 @@
+package pkg100days;
+public class day37 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(" *");
+            }System.out.println();
+        }
+    }   
+}

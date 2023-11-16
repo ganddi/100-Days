06c4a280decf5de0645467f0b89f039fe34dfa24
ganddi <kooganddi@gmail.com>
2023-11-16 08:44:46 +0800

    Membuat pola x menggunakan perulangan for

---
+++ day 39.java
@@ -0,0 +1,15 @@
+package pkg100days;
+public class day39 {
+    public static void main(String[] args) {
+        int z = 5;
+        for (int i = 1; i <= z ; i++) {
+            for (int j = 1; j <= z; j++) {
+               if(i == j || i + j == z + 1 ){
+                   System.out.print(" *");
+               }else{
+                   System.out.print("  ");
+               }
+            }System.out.println();
+        }
+    } 
+}

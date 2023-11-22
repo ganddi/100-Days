e43dcaae36073cc0c40380d7ac4278e57b609684
ganddi <kooganddi@gmail.com>
2023-11-22 21:15:49 +0800

    Deklarasi Array bertipe non-primitif/reference (bertipe class)

---
+++ day 45.java
@@ -0,0 +1,11 @@
+package pkg100days;
+public class day45 {
+    public static void main(String[] args) {
+        String[] namaMahasiswa = {"gandi", "greis", "avika", "ria", "rifqah"};
+
+        System.out.println("Nama Mahasiswa:");
+        for (int i = 0; i < namaMahasiswa.length; i++) {
+            System.out.println(namaMahasiswa[i]);
+        }
+    }
+}

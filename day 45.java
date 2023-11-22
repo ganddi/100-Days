eae2e59549c042f65af4423459db215478449327
gandi <146618812+ganddi@users.noreply.github.com>
2023-11-22 21:23:03 +0800

    Deklarasi Array bertipe non-primitif/reference (bertipe class)

--- day 45.java
+++ day 45.java
@@ -1,11 +1,11 @@
 package pkg100days;
 public class day45 {
     public static void main(String[] args) {
-        String[] namaMahasiswa = {"gandi", "greis", "avika", "ria", "rifqah"};
+        String[] namaMenti = {"gandi", "greis", "avika", "ria", "rifqah"};
 
         System.out.println("Nama Mahasiswa:");
-        for (int i = 0; i < namaMahasiswa.length; i++) {
-            System.out.println(namaMahasiswa[i]);
+        for (int i = 0; i < namaMenti.length; i++) {
+            System.out.println(namaMenti[i]);
         }
     }
 }

8acee159a503a00a32dcae0d51c5e53f3e5a4315
ganddi <kooganddi@gmail.com>
2023-11-25 22:50:28 +0800

    index array

---
+++ day 48.java
@@ -0,0 +1,12 @@
+package pkg100days;
+public class day48 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 40, 80, 160};
+
+        System.out.println("Isi Array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Index " + i + ": " + array[i]);
+        }
+    }
+    
+}

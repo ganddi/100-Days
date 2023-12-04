ff6cc205598e44a27f2306eb69068116ba4b8607
ganddi <kooganddi@gmail.com>
2023-12-04 21:28:16 +0800

    array 2d

---
+++ day 57.java
@@ -0,0 +1,13 @@
+package pkg100days;
+public class day57 {
+    public static void main(String[] args) {
+        String[][] array = {
+        {"A","B","C"},
+        {"D","E","F"},
+        {"G","H","I"} 
+    };
+        System.out.println(array[2][1]);
+        System.out.println(array[1][1]);
+    }
+    
+}

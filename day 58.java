a847c44b2c292d91914e7e55a150e920f299feaf
ganddi <kooganddi@gmail.com>
2023-12-05 20:56:01 +0800

    mencetak array 2d menggunakan for

---
+++ day 58.java
@@ -0,0 +1,17 @@
+package pkg100days;
+public class day58 {
+    public static void main(String[] args) {
+        int[][] array = {
+            {1,2},
+            {3,4},
+            {5,6}
+        };
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]);
+            }System.out.println();
+        }
+    
+    }
+    
+}

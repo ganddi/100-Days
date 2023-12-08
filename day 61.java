8e74d405c0fbbae2267b8254d819a2ab9213e7d3
ganddi <kooganddi@gmail.com>
2023-12-08 22:01:04 +0800

    mencetak array 1d menggunakan for each

---
+++ day 61.java
@@ -0,0 +1,11 @@
+package pkg100days;
+public class day61 {
+    public static void main(String[] args) {
+        double[] array = {34.4,26.3,87.5,64.4,79.9};
+        
+        for(double j : array){
+            System.out.println(j);
+        }
+    }
+    
+}

8acae4601ebb324c96ce397fc6a62f5fff8eafc1
ganddi <kooganddi@gmail.com>
2023-12-10 21:36:59 +0800

    mencetak array1d menggunakan while

---
+++ day 63.java
@@ -0,0 +1,10 @@
+package pkg100days;
+public class day63 {
+    public static void main(String[] args) {
+      int[] array = {87,43,17,47,21,46,29};
+       int i = 0;
+       while(i<array.length){
+           System.out.println(array[i]);
+           i++;
+       }
+    }}

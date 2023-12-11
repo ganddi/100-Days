df67e344b159f479608a0a89fb8fb74ea18792f6
ganddi <kooganddi@gmail.com>
2023-12-11 22:54:15 +0800

    array boolean 1d

---
+++ day 64.java
@@ -0,0 +1,26 @@
+package pkg100days;
+
+import java.util.Scanner;
+
+public class day64 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = scan.nextInt();
+
+        boolean[] statusMahasiswa = new boolean[jumlah];
+        
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Apakah Mahasiswa ke-" + (i + 1) + " lulus? (true/false): ");
+            statusMahasiswa[i] = scan.nextBoolean();
+        }
+        int lulus = 0;
+        for(boolean status : statusMahasiswa){
+            if(status){
+                lulus++;
+            }
+        }
+        System.out.println("Jumlah Mahasiswa yang lulus: "+ lulus);
+    }
+    
+}

8caa62cb9484f9e07112d9568eabaf7f0bce82b4
ganddi <kooganddi@gmail.com>
2023-12-16 23:21:42 +0800

    Mencetak array2d menggunakan method Arrays

---
+++ day 69.java
@@ -0,0 +1,13 @@
+package pkg100days;
+import java.util.Arrays;
+public class day69 {
+    public static void main(String[] args) {
+        int[][] array2d ={
+            {67,45},
+            {90,14},
+            {57,36}
+        };
+       System.out.println(Arrays.deepToString(array2d));
+    }
+    
+}

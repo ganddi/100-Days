281222810b0b4ceca774bff12889887819f6a813
ganddi <kooganddi@gmail.com>
2023-12-18 22:13:38 +0800

    mengurutkan nilai suatu array

---
+++ day 71.java
@@ -0,0 +1,13 @@
+package pkg100days;
+
+import java.util.Arrays;
+
+public class day71 {
+    public static void main(String[] args) {
+        int[] array = {36,47,87,55,52,21};
+        Arrays.sort(array);
+              for(int i: array){
+                 System.out.println(i);
+       }
+    }   
+}

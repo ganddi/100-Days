83d0cb256f485ad141e6b13cf12020c99423aee7
ganddi <kooganddi@gmail.com>
2023-12-29 21:04:22 +0800

    method parameter

---
+++ day 82.java
@@ -0,0 +1,10 @@
+package pkg100days;
+public class day82 {
+
+    static void fakultas(String prodi) {
+    System.out.println("Teknik "+prodi);}
+
+    public static void main(String[] args) {
+    fakultas("Informatika");
+    fakultas("Sipil");
+    fakultas("PWK");}}

26b63c55976e1397b749c18b10c841e8aacc5470
ganddi <kooganddi@gmail.com>
2023-12-31 18:15:19 +0800

    Method Parameter

---
+++ day 84.java
@@ -0,0 +1,12 @@
+package pkg100days;
+public class day84 {
+    static void ligaInggris(int klasemen,String klub) {
+        System.out.println(klasemen+klub);}
+   
+    public static void main(String[] args) {
+        ligaInggris(1," Liverpool");
+        ligaInggris(2," Aston Villa");
+        ligaInggris(3," Man City");
+        ligaInggris(4," Arsenal");
+        ligaInggris(5," Totenham");
+        ligaInggris(6," West Ham");   }}

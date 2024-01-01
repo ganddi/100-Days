798995a751f65f02bd7b061cdcc56801fdbae729
ganddi <kooganddi@gmail.com>
2024-01-01 15:11:24 +0800

    Aritmatika method parameter

---
+++ day 85.java
@@ -0,0 +1,11 @@
+package pkg100days;
+public class day85 {
+static void aritmatika (int x, int y) {
+    System.out.println("Hasil penjumlahan: " + (x+y));
+    System.out.println("Hasil pengurangan: " + (x-y));
+    System.out.println("Hasil perkalian  : " + (x*y));
+    System.out.println("Hasil pembagian  : " + (x/y));
+    System.out.println("Hasil Hasiilbagi : " + (x%y));}
+    
+public static void main(String[] args) {
+        aritmatika(9, 11);}}

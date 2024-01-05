eb606a756cbfb55392389ed8c02ef92d795b979d
ganddi <kooganddi@gmail.com>
2024-01-05 23:23:59 +0800

    Method parameter dan perulangan for

---
+++ day 89.java
@@ -0,0 +1,8 @@
+package pkg100days;
+public class day89 {
+static void perulangan (int angka){
+    for (int i = angka; i >= 1; i-=2) {
+        System.out.println(i);}}
+
+public static void main(String[] args) {
+        perulangan(39);}}

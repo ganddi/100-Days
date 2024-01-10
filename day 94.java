ff8a16a01c4d3b955f585b88d3304cbfad8e78ee
ganddi <kooganddi@gmail.com>
2024-01-10 23:15:47 +0800

    contains

---
+++ day 94.java
@@ -0,0 +1,12 @@
+package pkg100days;
+public class day94 {
+    public static void main(String[] args) {
+      String kalimat = "The quick brown fox jumps over the lazy dog";
+      
+      boolean hasil = kalimat.contains("fox");
+      
+      if(hasil) {System.out.println("Kata ada dalam kalimat");}
+      else {System.out.println("Kata tidak ada dalam kalimat");}
+    }
+    
+}
